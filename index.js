fedb29faece064728b7138b45949cd2bd568edd7
Colin Stevens <colinstevens06@gmail.com>
2021-04-07 10:36:02 -0400

    got the shell together and created some fadeToggle buttons and text blocks

---
+++ index.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+
+  $("#box-1").on("click", function () {
+    // if ()
+
+
+    $(".box-1").fadeToggle("slow")
+  })
+
+  $("#box-2").on("click", function () {
+    $(".box-2").fadeToggle("slow")
+  })
+
+  $("#box-3").on("click", function () {
+    $(".box-3").fadeToggle("slow")
+  })
+
+  $("#box-4").on("click", function () {
+    $(".box-4").fadeToggle("slow")
+  })
+
+});
